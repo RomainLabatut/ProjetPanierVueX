f58fcbcbe755724a8cd54e609808ae10673b12df
RomainLabatut <romain_lab@hotmail.fr>
2021-06-08 11:52:18 +0200

    second commit

--- src/views/home.vue
+++ src/views/home.vue
@@ -15,14 +15,6 @@
   <button v-on:click="increment" class="btn btn-primary">Go somewhere</button> 
   </div>
 </div>
-
- <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  <button v-on:click="increment" class="btn btn-primary">Go somewhere</button> 
-  </div>
-</div>
 </div>
 
 </template>
